f2a621fd23258fa24ec86d1a11dcf6ec90181e89
Guo Zhengjin <guo.zhengjin@gmail.com>
2019-11-05 22:39:08 +0800

    little by little

--- program.py
+++ program.py
@@ -1,3 +1,14 @@
+class Planner:
+    def __init__(self, total_storeys):
+        self.levels = [Level(i) for i in range(total_storeys)]
+        # index 0: upwards; index 1: downwards
+        self.direction_indicator = [False, False]
+
+    def press_button(self, level, direction):
+        # TODO turn the light on this level on
+        pass
+
+
 class Level:
     def __init__(self, number):
         self.number = number
@@ -9,8 +20,11 @@ class Level:
 
 
 class Person:
-    def __init__(self, start_level, destination_level):
+    def __init__(self, start_level, destination_level, planner):
         self.start_level = start_level
         self.destination_level = destination_level
+        self.direction = 0 if destination_level - start_level > 0 else 1
+        self.planner = planner
 
-
+    def press_button(self):
+        self.planner.press_button(self.start_level, self.direction)
